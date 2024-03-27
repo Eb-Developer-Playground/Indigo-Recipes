470e7ffc5167e1819e74d259f4783e1a98cd766d
SW-Muriu <sw.muriu@gmail.com>
2024-03-27 13:57:09 +0300

    Added the overall recipe

---
+++ src/app/recipe-management/singular-view-point/singular-view-point.component.ts
@@ -0,0 +1,68 @@
+import { Component, Input } from '@angular/core';
+import { SharedModule } from '../../zarchitecture/shared/shared/shared.module';
+import { FooterComponent } from '../../zarchitecture/layout/footer/footer.component';
+import { HeaderComponent } from '../../zarchitecture/layout/header/header.component';
+
+interface Recipe {
+  title: string;
+  imageUrl: string;
+  yield: string;
+  prepTime: string;
+  cookTime: string;
+  totalTime: string;
+  ingredients: string[];
+  instructions: string[];
+  tips: string[];
+}
+
+@Component({
+  selector: 'app-singular-view-point',
+  standalone: true,
+  imports: [
+    SharedModule,
+    FooterComponent,
+    HeaderComponent
+  ],
+  templateUrl: './singular-view-point.component.html',
+  styleUrl: './singular-view-point.component.scss'
+})
+export class SingularViewPointComponent {
+
+  // @Input() recipe!: Recipe;
+
+
+  recipe: Recipe = {
+    title: "One-Pan Lemon Garlic Chicken with Roasted Asparagus",
+    // imageUrl: "https://via.placeholder.com/300", 
+    imageUrl: "./../../../../assets/political.png",
+    yield: "4 servings",
+    prepTime: "10 minutes",
+    cookTime: "25 minutes",
+    totalTime: "35 minutes",
+    ingredients: [
+      "2 boneless, skinless chicken breasts",
+      "1 tablespoon olive oil",
+      "1 teaspoon dried oregano",
+      "1/2 teaspoon garlic powder",
+      "1/4 teaspoon salt",
+      "1/4 teaspoon black pepper",
+      "1 bunch asparagus, trimmed",
+      "1 lemon, sliced"
+    ],
+    instructions: [
+      "Preheat oven to 400°F (200°C). Lightly grease a baking sheet.",
+      "In a bowl, toss chicken breasts with olive oil, oregano, garlic powder, salt, and pepper.",
+      "Arrange chicken breasts on the prepared baking sheet. Scatter asparagus spears around the chicken.",
+      "Top with lemon slices.",
+      "Bake for 25 minutes, or until chicken is cooked through and asparagus is tender-crisp."
+    ],
+    tips: [
+      "For added flavor, marinate the chicken in the olive oil mixture for 30 minutes before baking.",
+      "You can substitute other vegetables for the asparagus, such as broccoli florets or bell peppers.",
+      "Serve with rice or quinoa for a complete meal."
+    ]
+  };
+
+  backgroundImageUrl: string = this.recipe.imageUrl
+
+}
